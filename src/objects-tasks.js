2bd2016213d126befcdc46e5b6bf27ded377c98e
JulietBrn <shishkina.ja@gmail.com>
2024-02-26 11:07:28 +0100

    feat: complete task partly

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,8 +17,10 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(/* obj */) {
-  throw new Error('Not implemented');
+function shallowCopy(obj) {
+  const newObj = {};
+  Object.assign(newObj, obj);
+  return newObj;
 }
 
 /**
@@ -32,8 +34,21 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  if (objects.length <= 1) {
+    return objects[0] || {};
+  }
+  const newObj = {};
+  objects.forEach((obj) => {
+    Object.entries(obj).forEach(([key, val]) => {
+      if (Object.prototype.hasOwnProperty.call(newObj, key)) {
+        newObj[key] += val;
+      } else {
+        newObj[key] = val;
+      }
+    });
+  });
+  return newObj;
 }
 
 /**
@@ -69,8 +84,8 @@ function removeProperties(obj, keys) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  return JSON.stringify(obj1) === JSON.stringify(obj2);
 }
 
 /**
